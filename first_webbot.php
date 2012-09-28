ddb77b83532e1cbb5f53af8a5294320d8650cbb0
lisawilliams <lisa@placeblogger.com>
2012-09-28 10:42:05 -0700

    A first bot.  It downloads a file.

---
+++ first_webbot.php
@@ -0,0 +1,33 @@
+<?php
+$page_title = "Hello World";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title><?php print($page_title) ?></title>
+</head>
+<body>
+
+<?php // 3.1 , filename first_webbot.php
+
+// error handling
+ini_set('display errors',1);  // Let me learn from my mistakes!
+error_reporting(E_ALL|E_STRICT); // Show all possible problems! 
+
+# Define the file you want to download
+
+$target - "http://www.WebbotsSpidersScreenScrapers.com/hello_world.html";
+$file_handle = fopen($target, "r");
+
+# Fetch the file 
+
+while (!feof($file_handle))
+		echo fgets($file_handle, 4096);
+fclose($file_handle);		
+
+?>
+
+</body>
+</html>
---
+++ stationery.php
@@ -0,0 +1,24 @@
+<?php
+$page_title = "";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title><?php print($page_title) ?></title>
+</head>
+<body>
+
+<?php // Script number , filename
+
+// error handling
+ini_set('display errors',1);  // Let me learn from my mistakes!
+error_reporting(E_ALL|E_STRICT); // Show all possible problems! 
+
+
+
+?>
+
+</body>
+</html>
